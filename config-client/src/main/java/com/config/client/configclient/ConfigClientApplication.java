eb3cd1fdc0128a668810e772481635186b84db85
fugithubs <1091565452@qq.com>
2020-01-02 17:46:48 +0800

    first submit config-client

---
+++ config-client/src/main/java/com/config/client/configclient/ConfigClientApplication.java
@@ -0,0 +1,31 @@
+package com.config.client.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hiConfigClient() {
+        return foo;
+    }
+    @Value("${city.touwn}")
+    private String city;
+    @RequestMapping(value = "/hicity")
+    public String hicity(){
+        return city;
+    }
+
+}
