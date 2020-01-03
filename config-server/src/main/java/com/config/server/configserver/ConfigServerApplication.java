bd945dc8cf258858faaf5fa12a76a887d017fdd3
fugithubs <1091565452@qq.com>
2020-01-03 15:15:55 +0800

    first submit config-client

--- config-server/src/main/java/com/config/server/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/config/server/configserver/ConfigServerApplication.java
@@ -3,11 +3,13 @@ package com.config.server.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author xiaorong.fu
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
